fee2625916d6bdfeec80fbdab2da1ddceabb6f45
Tuomas Tikkanen <tuomas.tikkanen@artific.ai>
2024-03-31 14:21:52 +0300

    Add Black formatter to development-time dependencies and format main.py with Black

--- src/app/main.py
+++ src/app/main.py
@@ -16,38 +16,40 @@ def detect_text(watermeter_image_path, session, rois):
     for roi in rois:
         # x, y, w, h
         topleft_col, topleft_row, width, height = roi
-        width_pos = Decimal(width)/Decimal(image_width)
-        height_pos = Decimal(height)/Decimal(image_height)
-        left_pos = Decimal(topleft_col)/Decimal(image_width)
-        top_pos = Decimal(topleft_row)/Decimal(image_height)
+        width_pos = Decimal(width) / Decimal(image_width)
+        height_pos = Decimal(height) / Decimal(image_height)
+        left_pos = Decimal(topleft_col) / Decimal(image_width)
+        top_pos = Decimal(topleft_row) / Decimal(image_height)
         region = {
-            'BoundingBox': {
-                'Width': float(width_pos),
-                'Height': float(height_pos),
-                'Left': float(left_pos),
-                'Top': float(top_pos)
+            "BoundingBox": {
+                "Width": float(width_pos),
+                "Height": float(height_pos),
+                "Left": float(left_pos),
+                "Top": float(top_pos),
             }
         }
         regions_of_interest.append(region)
-    print('Regions of interest: ' + str(regions_of_interest))
-    client = session.client('rekognition')
+    print("Regions of interest: " + str(regions_of_interest))
+    client = session.client("rekognition")
 
     # Open the image file in binary mode, encode it to base64
-    with open(watermeter_image_path, 'rb') as image_file:
-        encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+    with open(watermeter_image_path, "rb") as image_file:
+        encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
 
     response = client.detect_text(
         Image={
-            'Bytes': base64.b64decode(encoded_image),
+            "Bytes": base64.b64decode(encoded_image),
         },
-        Filters={
-            'RegionsOfInterest': regions_of_interest
-        }
+        Filters={"RegionsOfInterest": regions_of_interest},
     )
 
-    text_detections = response['TextDetections']
+    text_detections = response["TextDetections"]
     # Filter out the detections that are not digits and confidence is more than 90
-    digit_detections = [d['DetectedText'] for d in text_detections if d['DetectedText'].isdigit() and d['Confidence'] > 0 and d["Type"] == "WORD"]
+    digit_detections = [
+        d["DetectedText"]
+        for d in text_detections
+        if d["DetectedText"].isdigit() and d["Confidence"] > 0 and d["Type"] == "WORD"
+    ]
 
     return digit_detections
 
@@ -55,17 +57,22 @@ def detect_text(watermeter_image_path, session, rois):
 def main():
     # LOAD CONFIG FILE
     def load_config():
-        with open('config.yaml', 'r') as file:
+        with open("config.yaml", "r") as file:
             config = yaml.safe_load(file)
         return config
+
     config = load_config()
-    prerois = config['prerois'] = [tuple(roi) for roi in config['prerois']]
-    pregaugerois = config['pregaugerois'] = [tuple(roi) for roi in config['pregaugerois']]
-    postrois = config['postrois'] = [tuple(roi) for roi in config['postrois']]
-    postgaugerois = config['postgaugerois'] = [tuple(roi) for roi in config['postgaugerois']]
-    aws_profile = config['aws_profile']
-    aws_region = config['aws_region']
-    watermeter_image_path = config['watermeter_image_path']
+    prerois = config["prerois"] = [tuple(roi) for roi in config["prerois"]]
+    pregaugerois = config["pregaugerois"] = [
+        tuple(roi) for roi in config["pregaugerois"]
+    ]
+    postrois = config["postrois"] = [tuple(roi) for roi in config["postrois"]]
+    postgaugerois = config["postgaugerois"] = [
+        tuple(roi) for roi in config["postgaugerois"]
+    ]
+    aws_profile = config["aws_profile"]
+    aws_region = config["aws_region"]
+    watermeter_image_path = config["watermeter_image_path"]
     # Create a session using your AWS credentials from environment variables
     session = boto3.Session(profile_name=aws_profile, region_name=aws_region)
     watermeter_image_path = watermeter_image_path
@@ -78,6 +85,7 @@ def main():
 
     total_digits = preroisdigits + "." + postroisdigits
     print(f"total_digits: ", total_digits)
+
 
 if __name__ == "__main__":
     main()
